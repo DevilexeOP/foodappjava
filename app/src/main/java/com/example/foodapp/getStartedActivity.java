013ed1b8bea572921973ec837ddf2ac94f7c8aa5
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-03-07 23:33:41 +0530

    7th March

---
+++ app/src/main/java/com/example/foodapp/getStartedActivity.java
@@ -0,0 +1,30 @@
+package com.example.foodapp;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class getStartedActivity extends AppCompatActivity {
+private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_getstarted);
+
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getStartedActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
